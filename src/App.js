8a4e05a88aef22b80d847b079551d44f32528647
Momo241 <57633005+Mored241@users.noreply.github.com>
2023-03-11 17:29:56 +0000

    Deleting the unusual files

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello <code>It's Momo</code> how far ?
         </p>
         <a
           className="App-link"
