ca3b74554289e7d547e008c9fc53cdae6d0dab7c
Momo241 <57633005+Mored241@users.noreply.github.com>
2023-03-12 18:26:13 +0000

    test with tailwind

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello <code>It's Momo</code> how far ?
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p className='text-6xl'>Hello</p>
     </div>
   );
 }
